e7494da1ee07bbb268bc851487526e670ffffa70
Tung <dieptung20102005@gmail.com>
2025-12-16 08:04:06 +0700

    test

--- src/main/java/Student_CourseRegistration_Systeam.java
+++ src/main/java/Student_CourseRegistration_Systeam.java
@@ -9,6 +9,6 @@
 public class Student_CourseRegistration_Systeam {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Dcu laihuy");
     }
 }
